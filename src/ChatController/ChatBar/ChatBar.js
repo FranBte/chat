127eb0ac6c216f4df032bacedde76b8d957c2be2
Fran Borg <francesca.bte@outlook.com>
2019-10-14 20:26:02 +0200

    Add chatbar component

---
+++ src/ChatController/ChatBar/ChatBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ChatInput from "../ChatInput/ChatInput";
+import SendButton from "../SendButton/SendButton";
+import "./ChatBar.css";
+
+const ChatBar = props => {
+  const style = {
+    backgroundColor: "#3895d0",
+    width: "100%",
+    position: "fixed",
+    bottom: "0"
+    
+  };
+  return (
+    <section className="ChatBar" style={style}>
+        <ChatInput change={props.change} value={props.value}/>
+        <SendButton sendMessage={props.sendMessage}/>
+    </section>
+  );
+};
+
+export default ChatBar;
