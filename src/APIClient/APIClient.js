60bc82f4546d16b52ee8f9cf5cfe58638163cc8f
Fran Borg <francesca.bte@outlook.com>
2019-10-14 18:51:14 +0200

    Add async method for send messages

--- src/APIClient/APIClient.js
+++ src/APIClient/APIClient.js
@@ -3,6 +3,21 @@ class APIClient {
     this.url = `${url}?token=${token}`;
   }
 
+  async sendMessage(name, input) {
+    const response = await fetch(this.url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        author: name,
+        message: input
+      })
+    });
+    const content = await response.json();
+    return content;
+  }
 
 
 }
