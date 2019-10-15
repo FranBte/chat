785b820ec90db482f55935062fd0a7a42bec5596
Fran Borg <francesca.bte@outlook.com>
2019-10-15 17:29:20 +0200

    Add hover and active state to send button

--- src/ChatController/SendButton/SendButton.js
+++ src/ChatController/SendButton/SendButton.js
@@ -1,21 +1,10 @@
 import React from "react";
+import "./SendButton.css";
 
 const SendButton = props => {
-  const styleButton = {
-    border: "none",
-    height: "100%",
-    width: "10rem",
-    backgroundColor: "#fd866c",
-    fontSize: "2rem",
-    color: "white",
-    borderRadius: "0.5rem",
-    fontWeight: "500",
-    marginRight: "1rem",
-    fontFamily: `"Fira Sans", sans-serif`
-  };
   return (
     <button
-      style={styleButton}
+      className="SendButton"
       onClick={() => {
         props.sendMessage();
       }}
